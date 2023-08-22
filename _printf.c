c2e234643144679851a8f172aae36e2522fde02c
Eyad-95 <eyad952018@gmail.com>
2023-08-22 12:30:13 -0700

    done

---
+++ _printf.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+/**
+ * _printf - Entry point
+ * @format: character to be
+ * Return: Always 0
+ */
+
+int _printf(const char *format, ...)
+{
+	int n = 0;
+	int i;
+	va_list args;
+
+	va_start(args, format);
+
+	for (i = 0; format[i] != '\0'; i++)
+	{
+		if (format[i] == '%')
+		{
+			if (format[i + 1] == '%')
+				write(1, "%", 1);
+			else if (format[i + 1] == 'c')
+			{	char c = va_arg(args, int);
+
+				write(1, &c, 1);
+			} else if (format[i + 1] == 's')
+			{	const char *str = va_arg(args, const char *);
+				int k = 0;
+
+				while (str[k] != '\0')
+				{
+					write(1, &str[k], 1);
+					k++;
+					n++;
+				}
+			}
+		i++;
+		}
+		else
+			write(1, &format[i], 1);
+		n++;
+}
+
+	va_end(args);
+	write(1, "\n", 1);
+
+	return (n);
+}
