cf97c61a95de56143345a90ee797b3d9fdafe174
Eyad-95 <eyad952018@gmail.com>
2023-08-24 07:32:09 -0700

    negative and big numbers done

--- print_int.c
+++ print_int.c
@@ -1,32 +1,43 @@
 #include "main.h"
 
 /**
- * print_int - prints char
- * @n: character to be printed
- */
+* print_int - prints char
+* @n: character to be printed
+*/
 void print_int(int n)
 {
-	char h, tc, oc;
-	int t, o;
+char h, A[10];
+int i, j, k, p, flag = 0, flag2 = 0;
+char r, temp;
 
-	if (n > 0)
-	{
-		if (n < 10)
-		{
-			h = (char)n + '0';
-			write(1, &h, 1);
-		}
-		else
-		{
-			if (n >= 10 && n <= 99)
-			{
-				t = n / 10;
-				o = n % 10;
-				tc = (char)t + '0';
-				oc = (char)o + '0';
-				write(1, &tc, 1);
-				write(1, &oc, 1);
-			}
-		}
-	}
+if (n < 0)
+{   n *= -1;
+flag = 1;
+}
+if (n < 10)
+h = (char)n + '0';
+else
+{
+i = 0;
+while (n != 0)
+{
+r = n % 10;
+A[i++] = r + '0';
+n /= 10;
+}
+for (k = 0, p = i - 1; k < i / 2; k++, p--)
+{   temp = A[p];
+A[p] = A[k];
+A[k] = temp;
+}
+flag2 = 1;
+}
+if (flag == 1)
+write(1, "-", 1);
+if (flag2 == 1)
+{
+	for (j = 0; j < i; j++)
+		write(1, &A[j], 1);
+} else
+write(1, &h, 1);
 }
